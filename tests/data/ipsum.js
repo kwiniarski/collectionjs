79c8c0ec121930fce85a091211db783a82876d91
OpenShift guest <winiarski.krzysztof@gmail.com>
2013-06-25 08:20:30 -0400

    Separate dir for test data

---
+++ tests/data/ipsum.js
@@ -0,0 +1,192 @@
+define('ipsum', [], {
+	'id': 1,
+	'jsonrpc': '2.0',
+	'total': 5,
+	'result': [
+		{
+			'id': 1,
+			'guid': '2657c181-c539-4ee8-ad4a-aa8be01835ff',
+			'picture': 'http://placehold.it/32x32',
+			'age': 30,
+			'name': 'Chloe Daniels',
+			'gender': 'male',
+			'company': 'Aprama',
+			'phone': '871-478-3181',
+			'email': 'chloe@aprama.com',
+			'address': '39746, Corona, Orchard Street',
+			'about': 'In at molestie consequat nisl nisl, iriure nonummy hendrerit euismod commodo, suscipit zzril dolore iriure. Adipiscing, vel euismod duis ullamcorper nonummy, quis nonummy sit laoreet illum, in exerci vel. Molestie feugiat, molestie diam iriure quis dolor, dolor dolor ipsum facilisi ad, ut ex. Exerci dolore nibh, feugiat accumsan suscipit consectetuer.',
+			'registered': '1989-01-08T01:54:23 -01:00',
+			'tags': [
+				'lorem',
+				'sed',
+				'et',
+				'iusto',
+				'nulla',
+				'ad',
+				'molestie'
+			],
+			'friends': [
+				{
+					'id': 1,
+					'name': 'Bailey Galbraith'
+				},
+				{
+					'id': 2,
+					'name': 'Maya Haig'
+				},
+				{
+					'id': 3,
+					'name': 'Madelyn Walkman'
+				}
+			]
+		},
+		{
+			'id': 2,
+			'guid': 'f7cea211-2850-4c0d-8f4c-b1891ce5a329',
+			'picture': 'http://placehold.it/32x32',
+			'age': 25,
+			'name': 'Gabriella Day',
+			'gender': 'male',
+			'company': 'Textiqua',
+			'phone': '855-510-2007',
+			'email': 'gabriella@textiqua.com',
+			'address': '29024, Boise, Kenmare Streets',
+			'about': 'Eros blandit nonummy euismod exerci euismod, ipsum volutpat suscipit aliquip te, dolore lobortis facilisis wisi. Nibh, duis ad odio diam nostrud, suscipit delenit suscipit consectetuer nulla, diam duis dolore. Sed ut, dolore lobortis luptatum aliquip euismod, molestie vel vero minim duis, minim lorem. Sed nostrud exerci, laoreet minim erat zzril vel, consequat dignissim amet ipsum delenit, eu. Euismod esse sit minim, enim duis zzril ullamcorper et, at adipiscing suscipit et sit, consectetuer facilisi nonummy ullamcorper tation, nonummy sit vero augue dolore, facilisis volutpat ut velit. Dolore, minim tation praesent ullamcorper diam, sed laoreet praesent ut ipsum.',
+			'registered': '1993-02-26T11:23:13 -01:00',
+			'tags': [
+				'tation',
+				'odio',
+				'veniam',
+				'consequat',
+				'illum',
+				'ut',
+				'ut'
+			],
+			'friends': [
+				{
+					'id': 1,
+					'name': 'Sophie Oldridge'
+				},
+				{
+					'id': 2,
+					'name': 'Madison Nash'
+				},
+				{
+					'id': 3,
+					'name': 'Brooke Ogden'
+				}
+			]
+		},
+		{
+			'id': 3,
+			'guid': '8cbbac0f-8ce2-4858-a14d-2738814db461',
+			'picture': 'http://placehold.it/32x32',
+			'age': 35,
+			'name': 'Isabella Vance',
+			'gender': 'female',
+			'company': 'Textiqua',
+			'phone': '895-451-2908',
+			'email': 'isabella@textiqua.com',
+			'address': '14727, Aurora, Kenmare Streets',
+			'about': 'Nonummy facilisi wisi nulla dolor laoreet, amet suscipit aliquip nonummy amet, commodo duis commodo odio. Feugiat, in esse zzril ut dignissim, vel ut sed amet dolore, diam euismod lorem. Duis commodo, euismod consectetuer eum ipsum veniam, veniam dolore luptatum augue exerci, iusto luptatum. Suscipit luptatum amet, vel duis laoreet euismod tincidunt, dolor aliquam dolor diam iusto, consequat. Nulla magna te sed, nostrud hendrerit tation consequat in, in dolor zzril consequat dolore, lorem wisi tincidunt nisl nulla, et facilisi.',
+			'registered': '2010-08-19T04:20:59 -02:00',
+			'tags': [
+				'nisl',
+				'facilisis',
+				'nibh',
+				'molestie',
+				'in',
+				'ad',
+				'delenit'
+			],
+			'friends': [
+				{
+					'id': 1,
+					'name': 'Sophia Hodges'
+				},
+				{
+					'id': 2,
+					'name': 'Chloe Goodman'
+				},
+				{
+					'id': 3,
+					'name': 'Sophia Gustman'
+				}
+			]
+		},
+		{
+			'id': 4,
+			'guid': 'bd29879b-5601-454c-addb-19f2c3d18adf',
+			'picture': 'http://placehold.it/32x32',
+			'age': 35,
+			'name': 'Julia Hoggarth',
+			'gender': 'female',
+			'company': 'Steganoconiche',
+			'phone': '894-554-2704',
+			'email': 'julia@steganoconiche.com',
+			'address': '14282, Berkeley, Wall Street',
+			'about': 'Duis feugait aliquip qui augue iriure, ullamcorper sed nisl qui facilisi, luptatum esse accumsan ut. Enim, nonummy laoreet consectetuer ut quis, vero dolor facilisis iriure nonummy, vel ut luptatum. Ut sed, ut ut illum velit ipsum, et eros eum vulputate tation, dolore vel. Ad ipsum augue, suscipit laoreet laoreet vulputate in, ad adipiscing hendrerit luptatum consectetuer, autem. Elit nibh dolor dignissim, nonummy vero delenit duis accumsan, veniam eum erat duis feugiat, velit facilisi feugait facilisi at, ullamcorper vero sed elit sed, volutpat iusto delenit dolore..',
+			'registered': '1989-05-17T21:31:33 -02:00',
+			'tags': [
+				'exerci',
+				'amet',
+				'aliquam',
+				'facilisi',
+				'erat',
+				'tation',
+				'hendrerit'
+			],
+			'friends': [
+				{
+					'id': 1,
+					'name': 'Autumn Ogden'
+				},
+				{
+					'id': 2,
+					'name': 'Katherine Watson'
+				},
+				{
+					'id': 3,
+					'name': 'Ella Goldman'
+				}
+			]
+		},
+		{
+			'id': 5,
+			'guid': '2884b1a5-35bc-4fd5-adf8-70ad78f3b7b1',
+			'picture': 'http://placehold.it/32x32',
+			'age': 39,
+			'name': 'Alexa Young',
+			'gender': 'male',
+			'company': 'Titanirola',
+			'phone': '856-538-3033',
+			'email': 'alexa@titanirola.com',
+			'address': '31053, Gilbert, Horatio Streets',
+			'about': 'Vero velit duis odio blandit blandit, duis esse consequat autem nulla, vulputate minim dolor luptatum. Elit, nibh vel iriure iusto vero, vulputate nibh autem in blandit, nulla iusto amet. Luptatum accumsan, erat dignissim ipsum praesent vel, iusto eu ut vulputate eum, duis amet. Dignissim vulputate nonummy, ut erat zzril ut veniam, illum esse ipsum aliquam delenit, ex. Ea nonummy magna magna, enim dolore ad vel nulla, illum in consequat sed molestie, aliquam iusto odio lorem iusto, dolor dignissim wisi aliquam dolore, in diam consectetuer commodo. Nibh, et ut nisl ut lobortis, adipiscing ullamcorper lobortis tincidunt enim.',
+			'registered': '2003-12-04T07:18:13 -01:00',
+			'tags': [
+				'te',
+				'ut',
+				'vulputate',
+				'dolore',
+				'praesent',
+				'facilisi',
+				'ex'
+			],
+			'friends': [
+				{
+					'id': 1,
+					'name': 'Mariah Sheldon'
+				},
+				{
+					'id': 2,
+					'name': 'Autumn Ogden'
+				},
+				{
+					'id': 3,
+					'name': 'Kaitlyn Warren'
+				}
+			]
+		}
+	]
+});
